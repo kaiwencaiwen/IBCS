d6e8dde82cbf6af3749349423917493be1a0cc09
Kevin Chen <kevin.chen2b2020@stu.tes.tp.edu.tw>
2018-12-20 19:44:15 +0800

    Thu 12/20/2018

--- 0 - Other/test.py
+++ 0 - Other/test.py
@@ -1,1 +1,19 @@
-print([[0]*3]*4)
+public boolean haveThree(int[] nums) {
+  int count=0;
+  for (int i=0;i<nums.length-1;i++){
+    if(nums[i]==3 && nums[i+1]!=3){
+      count+=1;
+    }
+    else if(i==nums.length-1 && nums[i]==3 && nums[i-1]!=3){
+      count+=1;
+    }
+
+  }
+
+  if (count==3){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
