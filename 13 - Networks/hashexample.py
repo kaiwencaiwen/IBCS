88acace50296074e77e4e7d9f421974c416fb046
kaiwencaiwen <kevin.chen2b2020@stu.tes.tp.edu.tw>
2019-05-23 23:42:21 +0800

    Thu 05/23/2019

--- 13 - Networks/hashexample.py
+++ 13 - Networks/hashexample.py
@@ -1,4 +1,12 @@
 import hashlib
-bytes=b'password'
-result = hashlib.md5(bytes).hexdigest()
-print(result)
+target="3fafb2f44ee29e1cc0bd3128176945dc"
+with open('password_list.txt','r') as f:
+	plist=f.readlines()
+	for p in plist:
+		bytes=str.encode(p.strip('\n'))
+		result = hashlib.md5(bytes).hexdigest()
+		if result==target:
+			print("Password found:", p)
+			break
+
+print([i for i in range(0,10)])
