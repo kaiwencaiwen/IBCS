c3d7fa64987555ccdb530841f704980b5c46bf20
kaiwencaiwen <kevin.chen2b2020@stu.tes.tp.edu.tw>
2019-08-20 17:57:38 +0800

    Tue 08/20/2019

--- 0B - EE/opencv_test.py
+++ 0B - EE/opencv_test.py
@@ -1,7 +1,19 @@
+# Import numpy and OpenCV
+import numpy as np
 import cv2
-img= cv2.imread('lena.jpg', -1)
-print(img)
-cv2.imshow('image',img)
-cv2.waitKey(5000)
-cv2.destroyAllWindows()
-cv2.imwrite('lena_copy.png')
+ 
+# Read input video
+cap = cv2.VideoCapture('video.mp4')&nbsp;
+ 
+# Get frame count
+n_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) 
+ 
+# Get width and height of video stream
+w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) 
+h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+ 
+# Define the codec for output video
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+ 
+# Set up output video
+out = cv2.VideoWriter('video_out.mp4', fourcc, fps, (w, h))
